8453b0376194326334201257588e817d8b7fd1d5
tanlongbin <e0031490@u.nus.edu>
2019-11-27 16:52:04 +0800

    Add antd to package.json

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import logo from './logo.svg';
+import { Button } from 'antd';
 import './App.css';
+import 'antd/dist/antd.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 
 function App() {
   return (
     <div className="App">
+    <Router>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -18,7 +29,17 @@ function App() {
         >
           Learn React
         </a>
+        <Link to="/customer">
+        <Button>
+          Customer
+        </Button>
+        </Link>
+        
+        <Button href="/owner">
+          Drink Stall Owner
+        </Button>
       </header>
+      </Router>
     </div>
   );
 }
