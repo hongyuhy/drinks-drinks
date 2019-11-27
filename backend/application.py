e42cd8ddadda94ff9d20efd5781d311c28797572
Nicholas <nicholas_yeow@mymail.sutd.edu.sg>
2019-11-27 17:11:03 +0800

    updates

--- backend/application.py
+++ backend/application.py
@@ -1,10 +1,16 @@
 from flask import Flask
 import flask
-from flaskext.mysql import MySQL
+from flask_mysqldb import MySQL
+import services.get_drinks_from_db
+import services.add_drink_to_db
 
 app = Flask(__name__)
-mysql = MySQL()
-mysql.init_app(app)
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'root'
+app.config['MYSQL_DB'] = 'MyDB'
+
+mysql = MySQL(app)
 
 @app.route("/api/create_order", methods=['POST'])
 def create_order():
@@ -25,14 +31,32 @@ def get_menu():
 
 @app.route("/api/get_orders", methods=['GET'])
 def get_orders():
-    return {
-        "order": {
-            "id": "drink_id",
-            "drink_name": "milo",
-            "price": 1.0,
-            "status": "DElIVERED" # "UNDELIVERED"
-        }
-    }
+    return services.get_drinks_from_db()
+
+@app.route("/api/add_drinks", methods=['POST'])
+def add_drinks():
+    body = flask.request.get_json(force=True)
+    name = body["name"]
+    price = body["price"]
+    services.add_drink_to_db(name, price)
+    # TODO: some response
+    return
+
+
+@app.route("/api/complete_order", methods=['POST'])
+def complete_order():
+    #TODO
+    return
+
+
+@app.route("/test")
+def test():
+    cur = mysql.connection.cursor()
+    # cur.execute("INSERT INTO MyUsers(firstName, lastName) VALUES (%s, %s)", (firstName, lastName))
+    # cur.execute("")
+    mysql.connection.commit()
+    cur.close()
+    return
 
 
 
---
+++ backend/services.py
@@ -0,0 +1,13 @@
+def get_drinks_from_db():
+    return {
+        "order": {
+            "id": "drink_id",
+            "drink_name": "milo",
+            "price": 1.0,
+            "status": "DElIVERED" # "UNDELIVERED"
+        }
+    }
+
+def add_drink_to_db(name, price):
+    #TODO
+    return
